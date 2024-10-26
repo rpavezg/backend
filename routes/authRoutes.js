782ec3810ee17586b943b0415e61570ae72575d9
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-26 18:08:52 -0300

    upd authr

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const { register, login } = require('../controllers/authController');
+const { verifyToken } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/verify-token', verifyToken, (req, res) => {
+  res.status(200).json({ user: req.user });
+});
 
 module.exports = router;
