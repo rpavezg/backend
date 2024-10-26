8356755a47f8090dc0ec2ebb3b0c936b20132ec6
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-26 16:42:19 -0300

    upd back

--- routes/artistsRoutes.js
+++ routes/artistsRoutes.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const pool = require('../config/db');  // Conexión a la base de datos
+const pool = require('../config/db');
 const router = express.Router();
 
-// Ruta para obtener la lista de artistas
 router.get('/', async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM artista');  // Consulta la tabla 'artista' en la BD
-    res.json(result.rows);  // Devuelve los resultados en formato JSON
+    const result = await pool.query('SELECT * FROM artista');
+    res.json(result.rows);
   } catch (error) {
     console.error('Error al obtener los artistas:', error);
     res.status(500).json({ error: 'Error al obtener los artistas' });
--- server.js
+++ server.js
@@ -11,20 +11,19 @@ const app = express();
 app.use(cors({
   origin: 'https://frontgaleria.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
+  credentials: true,
 }));
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/protected', protectedRoutes);
-app.use('/api/artists', artistsRoutes);
+app.use('/api/auth', authRoutes); // Autenticación
+app.use('/api/protected', protectedRoutes); // Rutas protegidas
+app.use('/api/artists', artistsRoutes); // Rutas de artistas
 
 app.get('/', (req, res) => {
   res.send('API corriendo');
 });
 
 const PORT = process.env.PORT || 5000;
-
 if (process.env.NODE_ENV !== 'test') {
   app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
