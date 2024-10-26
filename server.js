7aa59595bfa0ef75a854e40f63933dc6abc02d3c
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-26 14:51:35 -0300

    upd server

--- server.js
+++ server.js
@@ -1,35 +1,28 @@
 const express = require('express');
 const cors = require('cors');
-require('dotenv').config();  // Cargar variables de entorno
+require('dotenv').config();
 
-const pool = require('./config/db');  // Conexión a la base de datos
-const authRoutes = require('./routes/authRoutes');  // Importar rutas de autenticación
-const protectedRoutes = require('./routes/protectedRoutes');  // Importar rutas protegidas
-const artistsRoutes = require('./routes/artistsRoutes'); // Importar la nueva ruta de artistas
+const pool = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const protectedRoutes = require('./routes/protectedRoutes');
+const artistsRoutes = require('./routes/artistsRoutes');
 
 const app = express();
 app.use(cors({
-  origin: 'https://frontgaleria.onrender.com', // Asegúrate de que esta URL sea la correcta
+  origin: 'https://frontgaleria.onrender.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true // Si necesitas cookies o autenticación basada en sesiones
+  credentials: true
 }));
 app.use(express.json());
 
-// Registrar las rutas de autenticación con prefijo '/api/auth'
 app.use('/api/auth', authRoutes);
-
-// Registrar las rutas protegidas con prefijo '/api/protected'
 app.use('/api/protected', protectedRoutes);
-
-// Registrar las rutas de artistas con prefijo '/api/artists'
 app.use('/api/artists', artistsRoutes);
 
-// Ruta de prueba para verificar que el servidor esté corriendo
 app.get('/', (req, res) => {
   res.send('API corriendo');
 });
 
-// Iniciar el servidor en el puerto especificado
 const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV !== 'test') {
@@ -38,4 +31,4 @@ if (process.env.NODE_ENV !== 'test') {
   });
 }
 
-module.exports = app;  // Exportar app para usar en las pruebas con Supertest
+module.exports = app;
