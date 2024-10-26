b3c1c60adf6029ac2e4d169b591399844f4b99fe
Rodrigo Pavez <pavez.rodrigo@gmail.com>
2024-10-26 18:17:46 -0300

    upd back

--- routes/protectedRoutes.js
+++ routes/protectedRoutes.js
@@ -14,4 +14,22 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Ruta protegida para obtener el perfil del usuario
+router.get('/profile', verifyToken, async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const result = await pool.query('SELECT email, nombre, apellido, level FROM users WHERE id = $1', [userId]);
+    const user = result.rows[0];
+
+    if (!user) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    res.json({ user });
+  } catch (error) {
+    console.error('Error al obtener el perfil del usuario:', error);
+    res.status(500).json({ error: 'Error al obtener el perfil' });
+  }
+});
+
 module.exports = router;
